a4b91461bd76d6145374a500e130fe98cbe661ad
О - 11 < - 11@DESKTOP-AK5R7PO>
2024-06-25 19:11:52 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Linq;
+
+namespace Lesson_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new Point(2, 5, 10);
+            point.MoveBy(5,2,0.5d);
+            Console.WriteLine(point.Text());
+
+            User user = new User("Ivanov", "Ivan", "Ivanovich",32);
+            user.Snfa();
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    public class User
+    {
+        private string surname;
+        private string name;
+        private string futhersname;
+        private int age;
+
+        public User(string surname, string name, string futhersname, int age)
+        {
+            this.surname = surname;
+            this.name = name;
+            this.futhersname = futhersname;
+            this.age = age;
+        }
+
+        public void Snfa()
+        {
+            Console.WriteLine(surname);
+            Console.WriteLine(name);
+            Console.WriteLine(futhersname);
+
+
+
+        }
+    }
+
+}
